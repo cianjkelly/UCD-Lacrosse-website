8caf12e083fc4f94feb4ed42740d3af79e095edc
Cian Kelly <cian.kelly@ucdconnect.ie>
2018-05-31 12:49:14 +0100

    Add files via upload

---
+++ other/input.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>
+        Thank You!
+    </title>
+</head>
+<body>
+<div>
+<?php
+
+$fname = $_GET["firstname"];
+$sname = $_GET["lastname"];
+$email = $_GET["email"];
+
+echo "thank you $fname $sname, we will be in touch soon!"
+?>
+
+Please click the logo to return to the homepage!
+<br>
+<br>
+<br>
+
+<a href="http://csserver.ucd.ie/~15386256/website/base.html"><img src="http://csserver.ucd.ie/~15386256/website/photos/LAX%20crest.jpg" display = "block" margin-top="60px" margin-right="auto" margin-left="auto" height="80px" width="80px"></a>
+</div>
+</body>
+</html>
+
+
+
+
